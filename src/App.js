aad4969d0de585f12fc632e9c81f31021ea61c56
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-14 15:56:42 +0100

    done

--- src/App.js
+++ src/App.js
@@ -36,9 +36,22 @@ const tweetsArray = [
 
 function App() {
   return (
-    <div className="App">
-      <Tweet tweet={tweetsArray[0]}/>
-    </div>
+    <div>
+      <div className="App">
+        <Tweet tweet={tweetsArray[0]}/>
+      </div>
+      <div className="App">
+        <Tweet tweet={tweetsArray[1]}/>
+      </div>
+      <div className="App">
+        <Tweet tweet={tweetsArray[2]}/>
+      </div>
+  </div>
+    /* tweetsArray.forEach(tweetDisplay => {
+      <div className="App">
+        <Tweet tweet={tweetDisplay[0]}/>
+      </div>
+    }); */
   );
 }
 
