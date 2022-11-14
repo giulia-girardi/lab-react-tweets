c0f8e855bfa35102728e52a18de506fe099acbdd
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-14 15:37:34 +0100

    done iter3 to iter7

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,33 +1,24 @@
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+import Message from "./Message";
+import Actions from "./Actions";
+
 function Tweet({tweet}) {
   return (
     <div className="tweet">
-      <img
-        src={tweet.user.image}
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={tweet.user.image}/>
 
       <div className="body">
+
         <div className="top">
-          <span className="user">
-            <span className="name">{tweet.user.name}</span>
-            <span className="handle">{tweet.user.handle}</span>
-          </span>
-
-          <span className="timestamp">{tweet.timestamp}</span>
+          <User userData={tweet.user}/>
+          <Timestamp time={tweet.timestamp}/>
         </div>
 
-        <p className="message">
-          {tweet.message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Message message={tweet.message} />
+        <Actions />
+      
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
